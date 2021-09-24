6de935320a7024c46e74a512a81b5faf8a2289e0
Paolo <Paolo@DESKTOP-EO3FFOD>
2021-09-24 17:39:56 -0500

    Agregar archivos de proyecto.

---
+++ AppTabDemo/DesignPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTabDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DesignPage : ContentPage
+    {
+        public DesignPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
